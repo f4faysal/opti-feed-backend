8d5816da3981adb461025d0858c08b26ce137961
f4faysal <f4faysals@gmail.com>
2023-11-25 16:34:07 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+const moduleRoutes = [
+  // ... routes
+ 
+];
+
+// moduleRoutes.forEach(route => router.use(route.path, route.routes));
+// export default router;
---
+++ src/enums/user.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+export enum ENUM_USER_ROLE {
+  STUDENT = 'student',
+  INSTRUCTOR = 'Instructor',
+}
